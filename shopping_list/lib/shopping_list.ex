a442fe4393b0426fcf0739a29c0ba75b04c6178f
kevin farias <kevin.farias.dev@gmail.com>
2022-05-02 14:34:00 -0300

    feat: finishing the external implementation of the api

--- shopping_list/lib/shopping_list.ex
+++ shopping_list/lib/shopping_list.ex
@@ -1,2 +1,8 @@
 defmodule ShoppingList do
+  alias ShoppingList.Type
+  alias ShoppingList.Impl.List
+
+  @spec calculate(Type.item_list(), Type.buyer_list()) ::
+          {:ok, Type.result_list_formatted()} | {:error, String.t()}
+  defdelegate calculate(item_list, buyer_list), to: List, as: :distribute_price
 end
